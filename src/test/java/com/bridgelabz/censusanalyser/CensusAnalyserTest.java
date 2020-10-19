fbb732977672d298718f28359dc2f84f275085a1
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-19 23:57:56 +0530

    Gaurav[ADDED]: Sad TestCase to raise custom Exception if Wrong Delimiter used

--- src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
@@ -14,6 +14,7 @@ public class CensusAnalyserTest {
     private static final String INDIA_STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
     private static final String WRONG_CSV_FILE_STATE = "./src/main/resources/IndiaStateCode.csv";
     private static final String WRONG_CSV_FILE_TYPE_STATE = "./src/test/resources/IndiaStateCode.txt";
+    private static final String WRONG_CSV_FILE_DELIMITER_STATE = "./src/test/resources/IndianStateCode.csv";
 
     /*Test Method for Welcome Message*/
     @BeforeClass
@@ -112,6 +113,18 @@ public class CensusAnalyserTest {
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CensusAnalyserException.class);
             censusAnalyser.loadIndiaStateData(WRONG_CSV_FILE_TYPE_STATE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+    /*Sad Test Case to return Custom Exception if StateCodeCSV file Delimiter is incorrect*/
+    @Test
+    public void givenWrongStateCodeFileWithWrongDelimiterReturnsException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaStateData(WRONG_CSV_FILE_DELIMITER_STATE);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
