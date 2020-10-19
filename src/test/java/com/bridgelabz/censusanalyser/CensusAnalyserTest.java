1c547f5e1017c24bfc65a354df6402b048c22853
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-19 22:58:05 +0530

    Gaurav[ADDED]: Sad TestCase to return Custom Exception if File is Incorrect

--- src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
@@ -3,10 +3,11 @@ package com.bridgelabz.censusanalyser;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
-
+    private static final String WRONG_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     /*Test Method for Welcome Message*/
     @BeforeClass
     public static void beforeClass() throws Exception {
@@ -23,6 +24,18 @@ public class CensusAnalyserTest {
             Assert.assertEquals(29, numOfRecords);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
+        }
+    }
+    /*Sad Test Case to return Custom Exception is file is incorrect*/
+    @Test
+    public void givenIndiaCensusDataWithWrongFileShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
     }
 }
