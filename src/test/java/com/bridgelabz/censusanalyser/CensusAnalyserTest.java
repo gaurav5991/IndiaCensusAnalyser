b52866809ba7b067a4b0ee3bf21b39b9dc56302f
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-19 22:51:56 +0530

    Gaurav[ADDED]:Test case to validate number of records

--- src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
@@ -2,13 +2,27 @@ package com.bridgelabz.censusanalyser;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class CensusAnalyserTest {
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+
     /*Test Method for Welcome Message*/
     @BeforeClass
     public static void beforeClass() throws Exception {
         CensusAnalyser censusAnalyser = new CensusAnalyser();
         boolean message = censusAnalyser.printWelcomeMessage();
         Assert.assertTrue(message);
+    }
+    /*Test Method to check number of entries*/
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29, numOfRecords);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
     }
 }
